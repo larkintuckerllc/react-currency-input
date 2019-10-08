2a3ad6dc4f4194d14c02fe234b679f6d6efbd107
John Tucker <jtucker@appirio.com>
2019-10-08 18:08:09 -0400

    Might Work

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,36 @@
-import React, { FC } from 'react';
+import React, { FC, KeyboardEvent, useCallback, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const VALID_FIRST = /^[1-9]{1}$/;
+const VALID_NEXT = /^[0-9]{1}$/;
+const DELETE_KEY_CODE = 8;
+
 const App: FC = () => {
+  const [value, setValue] = useState(0);
+  const handleKeyDown = useCallback((e: KeyboardEvent<HTMLDivElement>): void => {
+    const { key, keyCode } = e;
+    setValue(previousValue => {
+      if (
+        (previousValue === 0 && !VALID_FIRST.test(key)) ||
+        (previousValue !== 0 && !VALID_NEXT.test(key) && keyCode !== DELETE_KEY_CODE)
+      ) {
+        return previousValue;
+      }
+      const previousValueString = previousValue.toString();
+      let nextValue: number;
+      if (keyCode !== DELETE_KEY_CODE) {
+        const nextValueString: string = previousValue === 0 ? key : `${previousValueString}${key}`;
+        nextValue = Number.parseInt(nextValueString, 10);
+      } else {
+        const nextValueString = previousValueString.slice(0, -1);
+        nextValue = nextValueString === '' ? 0 : Number.parseInt(nextValueString, 10);
+      }
+      return nextValue;
+    });
+  }, []);
+  const valueDisplay = value / 100;
+
   return (
     <div className="App">
       <header className="App-header">
@@ -20,6 +48,10 @@ const App: FC = () => {
         >
           Learn React
         </a>
+        <div onKeyDown={handleKeyDown} role="textbox" tabIndex={0}>
+          {valueDisplay.toLocaleString('en-US', { style: 'currency', currency: 'USD' })}
+        </div>
+        <input />
       </header>
     </div>
   );
