3f2eeb90cc40ddd6466dc5277749351643d556b9
John Tucker <jtucker@appirio.com>
2019-10-08 16:18:05 -0400

    Fix 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { FC } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const App: React.FC = () => {
+const App: FC = () => {
   return (
     <div className="App">
       <header className="App-header">
